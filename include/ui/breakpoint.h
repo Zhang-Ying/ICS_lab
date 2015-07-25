6d77248e08ad9aa22de7fd3070a829a5d9a61114
root <root@debian>
2015-07-25 13:11:00 +0800

    head files

---
+++ include/ui/breakpoint.h
@@ -0,0 +1,17 @@
+#ifndef __UI_BREAKPOINT_H__
+#define __UI_BREAKPOINT_H__
+
+#include "common.h"
+
+#define INT3_CODE 0xcc
+
+typedef struct breakpoint {
+	int NO;
+	struct breakpoint *next;
+	/* TODO: Add more members if necessary */
+    char instr;
+	int address;
+	bool type;
+} BP;
+
+#endif
---
+++ include/ui/ui.h
@@ -0,0 +1,6 @@
+#ifndef __UI_H__
+#define __UI_H__
+
+enum { END, STOP, RUNNING, INT };
+extern int nemu_state;
+#endif
